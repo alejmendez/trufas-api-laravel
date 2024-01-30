8336d4ca951661fcf1cb8390ad045e0d12f3a3c7
Alejandro Méndez <alejmendez.87@gmail.com>
2024-01-30 00:12:37 -0300

    change error code jwt

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -16,14 +16,13 @@ class JwtMiddleware extends BaseMiddleware
             $user = JWTAuth::parseToken()->authenticate();
         } catch (Exception $e) {
             if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-                return response()->json(['status' => 'Token is Invalid'], 403);
+                return response()->json(['status' => 'Token is Invalid'], 401);
             }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-                return response()->json(['status' => 'Token is Expired'], 403);
+                return response()->json(['status' => 'Token is Expired'], 401);
             }else{
-                return response()->json(['status' => 'Authorization Token not found'], 403);
+                return response()->json(['status' => 'Authorization Token not found'], 401);
             }
         }
         return $next($request);
     }
 }
-
